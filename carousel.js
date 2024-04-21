75306aa2ed4f124f85a5224a0097019cb39542b0
kamin72 <a1616001@gmail.com>
2024-04-21 18:01:28 +0800

    將首頁輪播js寫在外部

--- carousel.js
+++ carousel.js
@@ -1,6 +1,6 @@
-window.onload = function () {
+window.onload = function carousel() {
   let slides = document.getElementsByClassName("carousel-item");
-
+  //將要播放的slide加上"active"的標記
   function addActive(slide) {
     slide.classList.add("active");
   }
@@ -10,18 +10,19 @@ window.onload = function () {
   }
 
   addActive(slides[0]);
+  //設定輪播間隔
   setInterval(function () {
     for (let i = 0; i < slides.length; i++) {
       if (i + 1 == slides.length) {
         addActive(slides[0]);
-        setTimeout(removeActive, 350, slides[i]);
+        setTimeout(removeActive, 1000, slides[i]);
         break;
       }
       if (slides[i].classList.contains("active")) {
-        setTimeout(removeActive, 350, slides[i]);
+        setTimeout(removeActive, 1000, slides[i]);
         addActive(slides[i + 1]);
         break;
       }
     }
-  }, 1500);
+  }, 1000);
 };
