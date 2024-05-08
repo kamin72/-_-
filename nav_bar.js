6ce1cc51c4878830a5b59755ea1361d34917af64
kamin72 <a1616001@gmail.com>
2024-05-08 16:41:09 +0800

    導覽列_點擊空白處可取消menu

--- nav_bar.js
+++ nav_bar.js
@@ -2,20 +2,23 @@ let nav = document.getElementById("nav"),
   fa_bars = document.getElementById("fa-bars"),
   fa_xmark = document.getElementById("fa-xmark");
 
-function barHidden() {
+function barDisplay() {
   if (window.innerWidth <= 767 && nav.classList.contains("hidden")) {
     fa_xmark.style.display = "block";
     fa_bars.style.visibility = "hidden";
     nav.classList.remove("hidden");
   }
 }
-fa_bars.onclick = barHidden;
+fa_bars.onclick = barDisplay;
 
-function barDisplay() {
+function barHidden() {
   if (window.innerWidth <= 767) {
     fa_xmark.style.display = "none";
     fa_bars.style.visibility = "visible";
     nav.classList.add("hidden");
   }
 }
-fa_xmark.onclick = barDisplay;
+fa_xmark.onclick = barHidden;
+
+//點擊空白處可以取消menu
+document.addEventListener("click", barHidden, true);
